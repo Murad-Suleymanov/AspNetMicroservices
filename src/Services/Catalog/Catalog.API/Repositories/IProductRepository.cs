74dba44325b64267a98082783cde1016b9113ef0
muradsuleymanov868 <muradsuleymanov868@gmail.com>
2022-01-20 16:04:22 +0400

    Add catalog api and basket api

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(string id);
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(string category);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
