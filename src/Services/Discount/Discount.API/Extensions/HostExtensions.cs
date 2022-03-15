f4d3e80e147ddfbbafa2f68ce3706a08d692518f
muradsuleymanov868 <muradsuleymanov868@gmail.com>
2022-03-15 14:19:57 +0400

    Add Grpc service and considering all project and plus Ordering.API added

--- src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -21,32 +21,35 @@ namespace Discount.API.Extensions
                 try
                 {
                     logger.LogInformation("Migrating postresql database");
+                    logger.LogInformation(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
-                    using var connection = new NpgsqlConnection
-                       (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-                    connection.Open();
-
-                    using var command = new NpgsqlCommand
+                    using (var connection = new NpgsqlConnection
+                       (configuration.GetValue<string>("DatabaseSettings:ConnectionString")))
                     {
-                        Connection = connection
-                    };
+                        connection.Open();
 
-                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
-                    command.ExecuteNonQuery();
+                        using var command = new NpgsqlCommand
+                        {
+                            Connection = connection
+                        };
 
-                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                        command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                        command.ExecuteNonQuery();
+
+                        command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
                                                                 ProductName VARCHAR(24) NOT NULL,
                                                                 Description TEXT,
                                                                 Amount INT)";
-                    command.ExecuteNonQuery();
+                        command.ExecuteNonQuery();
 
-                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
-                    command.ExecuteNonQuery();
+                        command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                        command.ExecuteNonQuery();
 
-                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
-                    command.ExecuteNonQuery();
+                        command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                        command.ExecuteNonQuery();
 
-                    logger.LogInformation("Migrated postresql database.");
+                        logger.LogInformation("Migrated postresql database.");
+                    }
                 }
                 catch (NpgsqlException ex)
                 {
